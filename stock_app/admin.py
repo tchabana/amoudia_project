46f36268ce8990d9c9233de7b10868e04ec1ea85
tchabana <hafiztchabana2004@gmail.com>
2023-12-06 19:08:28 +0000

    ajout des models dans le sit d'administration

--- stock_app/admin.py
+++ stock_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from stock_app.models import Achat, Commande, LigneCommande, Paiement, Personne, Produit, Vente
 
 # Register your models here.
+admin.site.register(Produit)
+admin.site.register(LigneCommande)
+admin.site.register(Personne)
+admin.site.register(Commande)
+admin.site.register(Achat)
+admin.site.register(Vente)
+admin.site.register(Paiement)
